1c49a65e7a7a54773088ea2d1dfbac57686a683a
IITincup <illia.bondarenko.00@gmail.com>
2021-09-22 02:46:17 -0600

    basic functionality and css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-/// placeholder
+function Q1Check() {
+  if (document.getElementById("q1yes").checked) {
+    document.getElementById("Q2").style.display = "block";
+  } else {
+    document.getElementById("Q2").style.display = "none";
+    document.getElementById("Q3").style.display = "none";
+    document.getElementById("passed").style.display = "none";
+    document.getElementById("notpassed").style.display = "block";
+  }
+}
+
+function Q2Check() {
+  if (document.getElementById("q2yes").checked) {
+    document.getElementById("Q3").style.display = "block";
+  } else {
+    document.getElementById("Q2").style.display = "none";
+    document.getElementById("Q3").style.display = "none";
+    document.getElementById("passed").style.display = "none";
+    document.getElementById("notpassed").style.display = "block";
+  }
+}
+
+function Q3Check() {
+  if (document.getElementById("q3yes").checked) {
+    document.getElementById("passed").style.display = "block";
+    document.getElementById("notpassed").style.display = "none";
+  } else {
+    document.getElementById("Q2").style.display = "none";
+    document.getElementById("Q3").style.display = "none";
+    document.getElementById("passed").style.display = "none";
+    document.getElementById("notpassed").style.display = "block";
+  }
+}
+
+function refresh() {
+  location.reload();
+}
