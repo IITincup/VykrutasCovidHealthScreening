8dce77adf6a7ef3fa452b6086239c20e056b2b3e
IITincup <illia.bondarenko.00@gmail.com>
2021-09-25 04:41:27 -0600

    Added a lot of css + added all the questions

--- js/main.js
+++ js/main.js
@@ -1,48 +1,179 @@
+var adult = true;
+var over12 = false;
 window.onload = function datetime() {
   let date = new Date();
-  let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];;
-  let formatedDate = date.getDate() + " " + month[date.getMonth()]; // + " " + date.getFullYear();
+  // let month = [
+  //   "Jan",
+  //   "Feb",
+  //   "Mar",
+  //   "Apr",
+  //   "May",
+  //   "June",
+  //   "July",
+  //   "Aug",
+  //   "Sept",
+  //   "Oct",
+  //   "Nov",
+  //   "Dec",
+  // ];
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let formatedDate = month[date.getMonth()] + " " + date.getDate();
   let minutes = date.getMinutes();
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  let formattedTime = date.getHours() + ":" + minutes;
-  document.getElementById("passedtext").innerHTML += "<br/>" + "<br/>" +  formatedDate + "<br/>" +formattedTime;
-  document.getElementById("notpassedtext").innerHTML += "<br/>" + "<br/>" + formatedDate + "<br/>" +formattedTime;
-  console.log(formattedTime);
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let formattedTime = "<br/>" + date.getHours() + ":" + minutes;
+  let times = document.querySelectorAll("h1.time");
+
+  times.forEach(
+    (time) => (time.innerHTML = formatedDate + " " + formattedTime)
+  );
 };
 
-function Q1Check() {
-  
-  if (document.getElementById("q1no").checked) {
-    document.getElementById("Q2").style.display = "block";
-  } else {
-    document.getElementById("Q2").style.display = "none";
-    document.getElementById("Q3").style.display = "none";
-    document.getElementById("passed").style.display = "none";
-    document.getElementById("notpassed").style.display = "block";
+function q1(who) {
+  switch (who) {
+    case "myself":
+      document.getElementById("age").style.display = "block";
+      document.getElementById("q1Under12").disabled = true;
+      document.getElementById("q1Over12").disabled = true;
+      break;
+    case "over12":
+      over12 = true;
+      adult = false;
+      document.getElementById("q1Under12").disabled = true;
+      document.getElementById("q1Myself").disabled = true;
+      document.getElementById("contact").style.display = "block";
+      break;
+    default:
+      document.getElementById("q1Over12").disabled = true;
+      document.getElementById("q1Myself").disabled = true;
+      adult = false;
+      document.getElementById("contact").style.display = "block";
+      break;
   }
 }
 
-function Q2Check() {
-  if (document.getElementById("q2no").checked) {
-    document.getElementById("Q3").style.display = "block";
-  } else {
-    document.getElementById("Q2").style.display = "none";
-    document.getElementById("Q3").style.display = "none";
-    document.getElementById("passed").style.display = "none";
-    document.getElementById("notpassed").style.display = "block";
+function age(age) {
+  switch (age) {
+    case "under":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("exception").style.display = "block";
+      break;
+    default:
+      document.getElementById("contact").style.display = "block";
+      document.getElementById("ageUnder").disabled = true;
+      break;
   }
 }
 
-function Q3Check() {
-  document.getElementById("Q2").style.display = "none";
-  document.getElementById("Q3").style.display = "none";
-  document.getElementById("passed").style.display = "none";
+function contact(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
+    default:
+      document.getElementById("travel").style.display = "block";
+      document.getElementById("contactYes").disabled = true;
 
-  if (document.getElementById("q3no").checked) {
-    document.getElementById("passed").style.display = "block";
-  } else {
-    document.getElementById("notpassed").style.display = "block";
-    
+      break;
+  }
+}
+
+function travel(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
+    default:
+      switch (adult) {
+        case true:
+          document.getElementById("fullSymptomps").style.display = "block";
+          document.getElementById("travelYes").disabled = true;
+
+          break;
+        default:
+          document.getElementById("seriousSymptomps").style.display = "block";
+          document.getElementById("travelYes").disabled = true;
+
+          break;
+      }
+      break;
+  }
+}
+
+function fullSymptomps(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
+    default:
+      document.getElementById("proof").style.display = "block";
+      document.getElementById("fullSymptomsYes").disabled = true;
+
+      break;
+  }
+}
+
+function seriousSymptomps(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
+    default:
+      document.getElementById("proof").style.display = "block";
+      document.getElementById("seriousSymptompsNo").disabled = true;
+
+      break;
+  }
+}
+
+function smallSymptomps(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
+    default:
+      switch (over12) {
+        case true:
+          document.getElementById("proof").style.display = "block";
+          document.getElementById("smallSymptompsNo").disabled = true;
+
+          break;
+        default:
+          document.getElementById("who").style.display = "none";
+          document.getElementById("passed").style.display = "block";
+          break;
+      }
+      break;
+  }
+}
+
+function proof(YN) {
+  switch (YN) {
+    case "yes":
+      document.getElementById("who").style.display = "none";
+      document.getElementById("passed").style.display = "block";
+      break;
+    default:
+      document.getElementById("who").style.display = "none";
+      document.getElementById("notpassed").style.display = "block";
+      break;
   }
 }
 
