075bb19eee1a4928abaef2be717133ff118f7ed5
IITincup <illia.bondarenko.00@gmail.com>
2021-09-24 21:20:59 -0600

    add Date and Time + changed font

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+window.onload = function datetime() {
+  let date = new Date();
+  let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];;
+  let formatedDate = date.getDate() + " " + month[date.getMonth()]; // + " " + date.getFullYear();
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  let formattedTime = date.getHours() + ":" + minutes;
+  document.getElementById("passedtext").innerHTML += "<br/>" + "<br/>" +  formatedDate + "<br/>" +formattedTime;
+  document.getElementById("notpassedtext").innerHTML += "<br/>" + "<br/>" + formatedDate + "<br/>" +formattedTime;
+  console.log(formattedTime);
+};
+
 function Q1Check() {
+  
   if (document.getElementById("q1no").checked) {
     document.getElementById("Q2").style.display = "block";
   } else {
@@ -21,14 +34,15 @@ function Q2Check() {
 }
 
 function Q3Check() {
+  document.getElementById("Q2").style.display = "none";
+  document.getElementById("Q3").style.display = "none";
+  document.getElementById("passed").style.display = "none";
+
   if (document.getElementById("q3no").checked) {
     document.getElementById("passed").style.display = "block";
-    document.getElementById("notpassed").style.display = "none";
   } else {
-    document.getElementById("Q2").style.display = "none";
-    document.getElementById("Q3").style.display = "none";
-    document.getElementById("passed").style.display = "none";
     document.getElementById("notpassed").style.display = "block";
+    
   }
 }
 
